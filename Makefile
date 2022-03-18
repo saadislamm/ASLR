716ca6285ebf3fd72b7e1da07067b880fe323b84
Saad Islam <sislam@wpi.edu>
2022-03-18 14:27:05 -0400

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc -o virtual_addr virtual_addr.c
+clean:
+	rm virtual_addr
---
+++ virtual_addr.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+
+	int a = 0;
+
+	printf("\nVirtual address: %p \n", &a);
+
+}
